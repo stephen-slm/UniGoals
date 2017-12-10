f71ec0dd33122a2ed876891b4d2166738de52234
tehstun <smmstephenmiller@gmail.com>
2017-12-10 00:37:40 +0000

    Version Bump 0.0.3

--- frontend/application/containers/store.js
+++ frontend/application/containers/store.js
@@ -3,7 +3,7 @@ import { createStore } from 'redux';
 import rootReducer from '../reducers/index';
 
 const defaultState = {
-  version: '0.0.2',
+  version: '0.0.3',
 };
 
 const store = createStore(rootReducer, defaultState);
