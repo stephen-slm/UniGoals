3004a6ee6ada807471d8844411a0b1103cd4cadc
Stephen <smmstephenmiller@gmail.com>
2018-01-30 16:21:13 +0000

    Version Bump 0.0.8

--- client/application/containers/store.js
+++ client/application/containers/store.js
@@ -3,7 +3,7 @@ import { createStore } from 'redux';
 import rootReducer from '../reducers/index';
 
 const defaultState = {
-  version: '0.0.7',
+  version: '0.0.8',
 };
 
 const store = createStore(rootReducer, defaultState);
