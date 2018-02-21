765255a87131bc2c16ce44907e968f0f66f131c4
Stephen <smmstephenmiller@gmail.com>
2018-02-21 00:19:43 +0000

    Stateless ranking
    ranking requires no state and is now pure / stateless
    using a utility class for the maths.

--- client/application/components/Summary/Ranking.jsx
+++ client/application/components/Summary/Ranking.jsx
@@ -4,7 +4,8 @@ import Typography from 'material-ui/Typography';
 import Paper from 'material-ui/Paper';
 import { withStyles } from 'material-ui/styles';
 import uuid from 'uuid/v4';
-import _ from 'lodash';
+
+import { calculateTopFiveRankings } from '../../utils/utils';
 
 const styles = (theme) => ({
   root: {
@@ -29,77 +30,27 @@ const styles = (theme) => ({
   },
 });
 
-class Ranking extends React.Component {
-  /**
-   * Applying the weighiting to all the units, getting the overal unit total and ordering
-   * the content based on this information with title, total and a link based on the
-   * current active address, this link being a hash link to the table contntes.
-   *
-   * Finally at the end this is reveresed as the lodash sortBy orders them based on
-   * lowest to highest and we need the vice vera.
-   * @param {object} data the unit data
-   * @param {object} history react-router history object
-   */
-  static calulateTopFive(data, history) {
-    if (_.size(data) === 0 || _.size(data[Object.keys(data)[0]]) === 0) {
-      return 0;
-    }
+const Ranking = (props) => {
+  const { classes } = props;
+  const ranking = calculateTopFiveRankings(props.units, props.history);
 
-    const listOfTotalGrades = [];
-
-    _.forEach(data, (unit, index) => {
-      let totalAchieved = 0;
-
-      _.forEach(unit.content, (unitContent) => {
-        if (
-          !_.isNil(unitContent.weighting) &&
-          !_.isNil(unitContent.achieved) &&
-          (unitContent.weighting !== '' && unitContent.achieved !== '')
-        ) {
-          if (parseFloat(unitContent.achieved) > 0) {
-            totalAchieved += parseFloat(unitContent.weighting) * parseFloat(unitContent.achieved);
-          }
-        }
-      });
-
-      listOfTotalGrades.push({
-        title: unit.title,
-        total: parseFloat(totalAchieved / 100).toFixed(2),
-        link: `${history.location.search}#${index}`,
-      });
-    });
-
-    return _.reverse(_.sortBy(listOfTotalGrades, (o) => o.total));
-  }
-
-  constructor() {
-    super();
-
-    this.state = {};
-  }
-
-  render() {
-    const { classes } = this.props;
-    const ranking = Ranking.calulateTopFive(this.props.units, this.props.history);
-
-    return (
-      <Paper style={{ height: this.props.height * 40 }} className={classes.root} elevation={1}>
-        <Typography className={classes.title} component="p">
-          Unit Ranking
-        </Typography>
-        <Typography className={classes.wrapper} component="div">
-          {ranking.map((rank, index) => (
-            <Typography key={uuid()} className={classes.entry} component="span">
-              <Typography className={classes.link} href={String(rank.link)} component="a">
-                {index + 1}. {rank.title}
-              </Typography>
+  return (
+    <Paper style={{ height: props.height * 40 }} className={classes.root} elevation={1}>
+      <Typography className={classes.title} component="p">
+        Unit Ranking
+      </Typography>
+      <Typography className={classes.wrapper} component="div">
+        {ranking.map((rank, index) => (
+          <Typography key={uuid()} className={classes.entry} component="span">
+            <Typography className={classes.link} href={String(rank.link)} component="a">
+              {index + 1}. {rank.title}
             </Typography>
-          ))}
-        </Typography>
-      </Paper>
-    );
-  }
-}
+          </Typography>
+        ))}
+      </Typography>
+    </Paper>
+  );
+};
 
 Ranking.propTypes = {
   classes: PropTypes.shape({}).isRequired,
--- client/application/utils/utils.js
+++ client/application/utils/utils.js
@@ -1,10 +1,57 @@
-function isMobileDevice() {
+import _ from 'lodash';
+
+export function isMobileDevice() {
   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     return true;
   }
   return false;
 }
 
-export { isMobileDevice };
+export function getAchievedTotalFromUnit(unit) {
+  let total = 0;
+
+  _.forEach(unit.content, (content) => {
+    if (
+      !_.isNil(content.weighting) &&
+      !_.isNil(content.achieved) &&
+      (content.weighting !== '' && content.achieved !== '')
+    ) {
+      if (parseFloat(content.achieved) > 0) {
+        total += parseFloat(content.weighting) * parseFloat(content.achieved);
+      }
+    }
+  });
+
+  return total;
+}
+
+/**
+ * Applying the weighiting to all the units, getting the overal unit total and ordering
+ * the content based on this information with title, total and a link based on the
+ * current active address, this link being a hash link to the table contntes.
+ *
+ * Finally at the end this is reveresed as the lodash sortBy orders them based on
+ * lowest to highest and we need the vice vera.
+ * @param {object} units the unit data
+ * @param {object} history react-router history object
+ */
+export function calculateTopFiveRankings(units, history) {
+  // return early if the size of the data is either 0 on the units or first unit data.
+  if (_.size(units) <= 0 || _.size(units[Object.keys(units)[0]]) <= 0) {
+    return 0;
+  }
+
+  const totalGradesList = _.map(units, (unit, index) => {
+    const totalAchieved = getAchievedTotalFromUnit(unit);
+
+    return {
+      title: unit.title,
+      total: totalAchieved,
+      link: `${history.location.search}#${index}`,
+    };
+  });
+
+  return _.reverse(_.sortBy(totalGradesList, (o) => o.total));
+}
 
 export default isMobileDevice;
