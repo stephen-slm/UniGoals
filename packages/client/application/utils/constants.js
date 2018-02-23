d3b7db4368dbeef624d8b158085cc7b8f166e5ef
Stephen <smmstephenmiller@gmail.com>
2018-02-23 23:58:23 +0000

    old colors
    Removed unused colors that where used or when charts where shown.

--- packages/client/application/utils/constants.js
+++ packages/client/application/utils/constants.js
@@ -2,16 +2,6 @@
 export const YEAR = { TITLE: { MIN: 4, MAX: 20 }, MAX: 5, MIN: 1 };
 
 export const TABLE = {
-  COLORS: [[98, 19, 98], [0, 159, 227]],
-  COMPLETE_COLORS: [
-    'rgb(255, 99, 132)',
-    'rgb(255, 205, 86)',
-    'rgb(54, 162, 235)',
-    'rgb(255, 159, 64)',
-    'rgb(75, 192, 192)',
-    'rgb(153, 102, 255)',
-    'rgb(201, 203, 207)',
-  ],
   NAME: {
     MAX: 12,
     MIN: 4,
